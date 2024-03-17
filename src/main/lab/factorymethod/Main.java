8cc28e0af7ee0511324b31c3b0c36af7bf576c46
groute63 <startref@yandex.ru>
2024-03-17 10:08:28 +0400

    Реализовал клонирование

--- src/main/lab/factorymethod/Main.java
+++ src/main/lab/factorymethod/Main.java
@@ -6,24 +6,22 @@ import main.model.vehicle.Vehicle;
 import main.model.vehicle.VehicleUtil;
 
 public class Main {
-    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException {
+    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException, CloneNotSupportedException {
         Vehicle v1 = VehicleUtil.createInstance("b1", 10);
         VehicleUtil.setTransportFactory(FactoryType.BIKE_FACTORY);
         Vehicle v2 = VehicleUtil.createInstance("b2",5);
 
         Vehicle v3 = v1.clone();
-        v3.setBrand("b3");
         v3.setModelName("model2", "newModel2");
         v3.deleteModel("model1");
         VehicleUtil.printModelNames(v3);
         VehicleUtil.printModelNames(v1);
 
-        Vehicle v4 = v2.clone();
-        v4.setBrand("b3");
-        v4.setModelName("model2", "newModel2");
-        v4.deleteModel("model1");
-        VehicleUtil.printModelNames(v4);
-        VehicleUtil.printModelNames(v2);
 
+        Vehicle v2c = v2.clone();
+        v2c.setModelName("model2", "newModel2");
+        v2c.deleteModel("model1");
+        VehicleUtil.printModelNames(v2);
+        VehicleUtil.printModelNames(v2c);
     }
 }
